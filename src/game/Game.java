e14f86192d0ce115e9fff98ba16b6462f32d9d1e
saggeseniccolo <snicco2004@gmail.com>
2022-12-17 20:51:04 +0100

    qwesad

--- src/game/Game.java
+++ src/game/Game.java
@@ -12,256 +12,277 @@ import javax.sound.sampled.*;
 
 public class Game extends JPanel implements KeyListener {
 
-    private static Personaggio personaggio;
-    private static int width = 400;
-    private static int heigth = 400;
-    private String soundName = "audio/dig.wav";
+	private static Personaggio personaggio;
+	private static int width = 400;
+	private static int heigth = 400;
+	private String soundName = "audio/dig.wav";
 
-    static Ostacoli muroSopra = new Ostacoli(0, 0, 400, 10);
-    static Ostacoli muroSotto = new Ostacoli(0, 353, 400, 10);
-    static Ostacoli muroDestro = new Ostacoli(376, 0, 10, 400);
-    static Ostacoli muroSinistro = new Ostacoli(0, 0, 10, 400);
+	static Ostacoli muroSopra = new Ostacoli(0, 0, 400, 10);
+	static Ostacoli muroSotto = new Ostacoli(0, 353, 400, 10);
+	static Ostacoli muroDestro = new Ostacoli(376, 0, 10, 400);
+	static Ostacoli muroSinistro = new Ostacoli(0, 0, 10, 400);
 
-    static Random randomico1 = new Random();
-    static Random randomico2 = new Random();
+	static Random randomico1 = new Random();
+	static Random randomico2 = new Random();
 
-    static int xTra = ((randomico1.nextInt(37)) + 1) * 10;
-    static int yTra = ((randomico2.nextInt(35)) + 1) * 10;
+	static int xTra = ((randomico1.nextInt(37)) + 1) * 10;
+	static int yTra = ((randomico2.nextInt(35)) + 1) * 10;
 
-    static Traguardo traguardo = new Traguardo(xTra, yTra);
-    long startTime = System.currentTimeMillis();
+	static Traguardo traguardo = new Traguardo(xTra, yTra);
+	long startTime = System.currentTimeMillis();
 
-    static ArrayList<Integer> posizioni = new ArrayList<>();
-    static ArrayList<Ostacoli> nuovoArray = new ArrayList<>();
-    static int z = 0;
+	static ArrayList<Integer> posizioni = new ArrayList<>();
+	static ArrayList<Ostacoli> nuovoArray = new ArrayList<>();
+	static int z = 0;
 
-    public Game() {
-        personaggio = new Personaggio(10, 10, 10);
-    }
+	static JFrame frame = new JFrame();
 
-    public static void main(String[] args) {
-        boolean controlla = false;
-        while (xTra == 10 && yTra == 10) {
-            xTra = ((randomico1.nextInt(38)) + 1) * 10;
-            yTra = ((randomico2.nextInt(36)) + 1) * 10;
-            controlla = true;
-        }
-        if (xTra > 360) {
-            xTra = 366;
-            controlla = true;
-        }
+	public Game() {
+		personaggio = new Personaggio(10, 10, 10);
+	}
 
-        if (yTra > 340) {
-            yTra = 343;
-            controlla = true;
-        }
+	public static void main(String[] args) {
+		boolean controlla = false;
+		while (xTra == 10 && yTra == 10) {
+			xTra = ((randomico1.nextInt(38)) + 1) * 10;
+			yTra = ((randomico2.nextInt(36)) + 1) * 10;
+			controlla = true;
+		}
+		if (xTra > 360) {
+			xTra = 366;
+			controlla = true;
+		}
 
-        if (controlla) {
-            traguardo.setX(xTra);
-            traguardo.setY(yTra);
-        }
+		if (yTra > 340) {
+			yTra = 343;
+			controlla = true;
+		}
 
-        int[][] array = creaSchema();
+		if (controlla) {
+			traguardo.setX(xTra);
+			traguardo.setY(yTra);
+		}
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (array[i][j] == 1) {
-                    nuovoArray.add(new Ostacoli(posizioni.get(z), posizioni.get(z + 1)));
-                    z = z + 2;
-                }
-            }
-        }
+		int[][] array = creaSchema();
 
-        JFrame frame = new JFrame();
-        Game game = new Game();
-        for (int i = 0; i < nuovoArray.size(); i++) {
-            frame.add(nuovoArray.get(i));
-        }
-        frame.add(personaggio);
-        frame.add(muroSopra);
-        frame.add(muroSotto);
-        frame.add(muroDestro);
-        frame.add(muroSinistro);
-        frame.add(traguardo);
-        frame.add(game);
-        frame.addKeyListener(game);
-        frame.setSize(width, heigth);
-        frame.setLocation(450, 150);
-        frame.setResizable(false);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				if (array[i][j] == 1) {
+					nuovoArray.add(new Ostacoli(posizioni.get(z), posizioni.get(z + 1)));
+					z = z + 2;
+				}
+			}
+		}
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        if (keyCode == KeyEvent.VK_UP) {
+		Game game = new Game();
+		for (int i = 0; i < nuovoArray.size(); i++) {
+			frame.add(nuovoArray.get(i));
+		}
+		frame.add(personaggio);
+		frame.add(muroSopra);
+		frame.add(muroSotto);
+		frame.add(muroDestro);
+		frame.add(muroSinistro);
+		frame.add(traguardo);
+		frame.add(game);
+		frame.addKeyListener(game);
+		frame.setSize(width, heigth);
+		frame.setLocation(450, 150);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
-            boolean move = true;
-            for (int i = 0; i < nuovoArray.size(); i++) {
-                if (nuovoArray.get(i).checkCollisionTop(personaggio)) {
-                    move = false;
-                    break;
-                }
-            }
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		if (keyCode == KeyEvent.VK_UP) {
 
-            if (move == true) {
-                personaggio.moveUp();
-            }
+			boolean move = true;
+			for (int i = 0; i < nuovoArray.size(); i++) {
+				if (nuovoArray.get(i).checkCollisionTop(personaggio)) {
+					move = false;
+					break;
+				}
+			}
 
-        } else if (keyCode == KeyEvent.VK_DOWN) {
-            boolean move = true;
+			if (move == true) {
+				personaggio.moveUp();
+			}
 
-            for (int i = 0; i < nuovoArray.size(); i++) {
-                if (nuovoArray.get(i).checkCollisionBottom(personaggio)) {
-                    move = false;
-                    break;
-                }
+		} else if (keyCode == KeyEvent.VK_DOWN) {
+			boolean move = true;
 
-            }
+			for (int i = 0; i < nuovoArray.size(); i++) {
+				if (nuovoArray.get(i).checkCollisionBottom(personaggio)) {
+					move = false;
+					break;
+				}
 
-            if (move == true) {
-                personaggio.moveDown();
-            }
-        } else if (keyCode == KeyEvent.VK_LEFT) {
-            boolean move = true;
+			}
 
-            for (int i = 0; i < nuovoArray.size(); i++) {
-                if (nuovoArray.get(i).checkCollisionLeft(personaggio)) {
-                    move = false;
-                    break;
-                }
-            }
+			if (move == true) {
+				personaggio.moveDown();
+			}
+		} else if (keyCode == KeyEvent.VK_LEFT) {
+			boolean move = true;
 
-            if (move == true) {
-                personaggio.moveLeft();
-            }
-        } else if (keyCode == KeyEvent.VK_RIGHT) {
-            boolean move = true;
+			for (int i = 0; i < nuovoArray.size(); i++) {
+				if (nuovoArray.get(i).checkCollisionLeft(personaggio)) {
+					move = false;
+					break;
+				}
+			}
 
-            for (int i = 0; i < nuovoArray.size(); i++) {
-                if (nuovoArray.get(i).checkCollisionRight(personaggio)) {
-                    move = false;
-                    break;
-                }
-            }
+			if (move == true) {
+				personaggio.moveLeft();
+			}
+		} else if (keyCode == KeyEvent.VK_RIGHT) {
+			boolean move = true;
 
-            if (move == true) {
-                personaggio.moveRight();
-            }
-        } else if (keyCode == KeyEvent.VK_C) {
-            Random random = new Random();
-            personaggio.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
-        }
+			for (int i = 0; i < nuovoArray.size(); i++) {
+				if (nuovoArray.get(i).checkCollisionRight(personaggio)) {
+					move = false;
+					break;
+				}
+			}
 
-        if (personaggio.getBounds().intersects(traguardo.getBounds())) {
-            // If the player has reached the end point, display a victory message and exit
-            // the game
-            long elapsedTime = System.currentTimeMillis() - startTime;
-            double elapsedTimeInSeconds = elapsedTime / 1000.0;
-            // int scelta = JOptionPane.showConfirmDialog(this, "Congratulazioni, sei
-            // fuggito da Catanzaro!\n Tempo totale: " + elapsedTimeInSeconds + "
-            // secondi"+"\nVuoi rigiocare?",);
-            int scelta = JOptionPane.showConfirmDialog(this,
-                    "Congratulazioni, sei fuggito da Catanzaro!\n Tempo totale: " + elapsedTimeInSeconds + " secondi"
-                            + "\nVuoi rigiocare?",
-                    "Fine gioco", JOptionPane.YES_NO_OPTION);
-            if (scelta == JOptionPane.YES_OPTION) {
-                startTime = System.currentTimeMillis();
-                personaggio.setX(10);
-                personaggio.setY(10);
-                repaint();
-            } else if (scelta == JOptionPane.NO_OPTION) {
-                System.exit(0);
-            } else if (scelta == JOptionPane.CLOSED_OPTION) {
-                startTime = System.currentTimeMillis();
-                personaggio.setX(10);
-                personaggio.setY(10);
-            }
+			if (move == true) {
+				personaggio.moveRight();
+			}
+		} else if (keyCode == KeyEvent.VK_C) {
+			Random random = new Random();
+			personaggio.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+		}
 
-        }
-        repaint();
-    }
+		if (personaggio.getBounds().intersects(traguardo.getBounds())) {
+			// If the player has reached the end point, display a victory message and exit
+			// the game
+			long elapsedTime = System.currentTimeMillis() - startTime;
+			double elapsedTimeInSeconds = elapsedTime / 1000.0;
+			// int scelta = JOptionPane.showConfirmDialog(this, "Congratulazioni, sei
+			// fuggito da Catanzaro!\n Tempo totale: " + elapsedTimeInSeconds + "
+			// secondi"+"\nVuoi rigiocare?",);
+			int scelta = JOptionPane.showConfirmDialog(this,
+					"Congratulazioni, sei fuggito da Catanzaro!\n Tempo totale: " + elapsedTimeInSeconds + " secondi"
+							+ "\nVuoi rigiocare?",
+					"Fine gioco", JOptionPane.YES_NO_OPTION);
+			if (scelta == JOptionPane.YES_OPTION) {
+				startTime = System.currentTimeMillis();
+				personaggio.setX(10);
+				personaggio.setY(10);
+				generaMappa();
+			} else if (scelta == JOptionPane.NO_OPTION) {
+				System.exit(0);
+			} else if (scelta == JOptionPane.CLOSED_OPTION) {
+				startTime = System.currentTimeMillis();
+				personaggio.setX(10);
+				personaggio.setY(10);
+			}
+		}
+		repaint();
+	}
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        personaggio.drawCharachter(g);
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		personaggio.drawCharachter(g);
 
-        muroDestro.draw(g);
-        muroSinistro.draw(g);
-        muroSotto.draw(g);
-        muroSopra.draw(g);
-        traguardo.draw(g);
-        for (int i = 0; i < nuovoArray.size(); i++) {
-            nuovoArray.get(i).draw(g);
-        }
-    }
+		muroDestro.draw(g);
+		muroSinistro.draw(g);
+		muroSotto.draw(g);
+		muroSopra.draw(g);
+		traguardo.draw(g);
+		for (int i = 0; i < nuovoArray.size(); i++) {
+			nuovoArray.get(i).draw(g);
+		}
+	}
 
-    @Override
-    public void keyTyped(KeyEvent ke) {
+	@Override
+	public void keyTyped(KeyEvent ke) {
 
-    }
+	}
 
-    @Override
-    public void keyReleased(KeyEvent ke) {
-        int keyCode = ke.getKeyCode();
-        if (keyCode == KeyEvent.VK_UP || keyCode == KeyEvent.VK_DOWN || keyCode == KeyEvent.VK_LEFT
-                || keyCode == KeyEvent.VK_RIGHT) {
-            try {
-                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName));
-                Clip clip = AudioSystem.getClip();
-                clip.open(audioInputStream);
-                Control[] controls = clip.getControls();
+	@Override
+	public void keyReleased(KeyEvent ke) {
+		int keyCode = ke.getKeyCode();
+		if (keyCode == KeyEvent.VK_UP || keyCode == KeyEvent.VK_DOWN || keyCode == KeyEvent.VK_LEFT
+				|| keyCode == KeyEvent.VK_RIGHT) {
+			try {
+				AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName));
+				Clip clip = AudioSystem.getClip();
+				clip.open(audioInputStream);
+				Control[] controls = clip.getControls();
 
-                for (Control control : controls) {
-                    if (control.getType() == FloatControl.Type.VOLUME) {
-                        FloatControl volumeControl = (FloatControl) control;
-                        volumeControl.setValue(0.1f);
-                        break;
-                    }
+				for (Control control : controls) {
+					if (control.getType() == FloatControl.Type.VOLUME) {
+						FloatControl volumeControl = (FloatControl) control;
+						volumeControl.setValue(0.1f);
+						break;
+					}
 
-                    clip.start();
+					clip.start();
+				}
+			} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-                }
+	static public int[][] creaSchema() {
+		int[][] array = new int[37][35];
 
-            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+		double randomNumber = 0;
 
-    static public int[][] creaSchema() {
-        int[][] array = new int[37][35];
+		for (int i = 0; i < array.length; i++) {
+			// Itero sugli elementi dell'array interno
+			for (int j = 0; j < array[i].length; j++) {
+				randomNumber = Math.random() * 8 - 1;
+				// Arrotondo il numero casuale a un intero
+				int randomInt = (int) Math.round(randomNumber);
+				randomInt = Math.abs(randomInt);
+				int controllo1 = ((i + 1) * 10);
+				int controllo2 = ((j + 1) * 10);
 
-        double randomNumber = 0;
+				if (controllo1 > 360) {
+					controllo1 = 366;
+				}
+				if (controllo2 > 340) {
+					controllo2 = 343;
+				}
+				if ((i == 0 && j == 0) || (controllo1 == traguardo.getX() && controllo2 == traguardo.getY())) {
+					randomInt = 0;
+				}
+				array[i][j] = randomInt;
+				if (randomInt == 1) {
+					posizioni.add(controllo1);
+					posizioni.add(controllo2);
+				}
+			}
+		}
+		return array;
+	}
 
-        for (int i = 0; i < array.length; i++) {
-            // Itero sugli elementi dell'array interno
-            for (int j = 0; j < array[i].length; j++) {
-                randomNumber = Math.random() * 8 - 1;
-                // Arrotondo il numero casuale a un intero
-                int randomInt = (int) Math.round(randomNumber);
-                randomInt = Math.abs(randomInt);
-                int controllo1 = ((i + 1) * 10);
-                int controllo2 = ((j + 1) * 10);
+	void generaMappa() {
+		int[][] array = creaSchema();
 
-                if (controllo1 > 360) {
-                    controllo1 = 366;
-                }
-                if (controllo2 > 340) {
-                    controllo2 = 343;
-                }
-                if ((i == 0 && j == 0) || (controllo1 == traguardo.getX() && controllo2 == traguardo.getY())) {
-                    randomInt = 0;
-                }
-                array[i][j] = randomInt;
-                if (randomInt == 1) {
-                    posizioni.add(controllo1);
-                    posizioni.add(controllo2);
-                }
-            }
-        }
-        return array;
-    }
+		for (int i = 0; i < nuovoArray.size(); i++) {
+			frame.remove(nuovoArray.get(i));
+		}
+
+		repaint();
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				if (array[i][j] == 1) {
+					nuovoArray.add(new Ostacoli(posizioni.get(z), posizioni.get(z + 1)));
+					z = z + 2;
+				}
+			}
+		}
+
+		for (int i = 0; i < nuovoArray.size(); i++) {
+			frame.add(nuovoArray.get(i));
+		}
+	}
 }
